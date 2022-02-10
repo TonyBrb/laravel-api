52f54881783f60ca160c699b08c43d783abb6b1c
TonyBrb <anthonybarbagallo95@gmail.com>
2022-02-10 16:18:57 +0100

    divisione fron end back end e chiamata axios per prendere i post

---
+++ resources/js/components/Posts.vue
@@ -0,0 +1,52 @@
+<template>
+  <main class="container">
+    <h2>I  miei post</h2>
+    <PostItem 
+      v-for="post in posts"
+      :key="post.id"
+      :post = 'post'
+    />
+  </main>
+</template>
+
+<script>
+
+import PostItem from './partials/PostItem.vue';
+
+export default {
+  name: 'Posts',
+  components:{
+    PostItem
+  },
+  data(){
+    return{
+      apiUrl: 'http://127.0.0.1:8000/api/posts',
+      posts: null
+    }
+  },
+
+  mounted(){
+    this.getPost();
+  },
+
+  methods:{
+    getPost(){
+      axios.get(this.apiUrl)
+      .then(res=>{
+        this.posts = res.data
+        console.log(this.posts);
+      })
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+  main{
+    padding: 30px 0;
+    h2{
+      margin-bottom: 30px;
+    }
+  }
+</style>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('guest.welcome');
+    return view('guest.home');
 })->name('home');
 
 Auth::routes();
@@ -29,3 +29,7 @@ Route::middleware('auth')
 
     Route::resource('/posts','PostsController');
 });
+
+Route::get('{any?}', function () {
+    return view('guest.home');
+})->where('any', '.*')->name('home');
